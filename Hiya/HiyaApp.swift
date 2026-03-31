98da59f1f0f9326ffc4e658444949e6c946b5afb
Jasmine <sandhujasmine@seasiainfotech.com>
2026-03-31 09:37:49 +0530

    First commit

---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Jasmine Kaur on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
